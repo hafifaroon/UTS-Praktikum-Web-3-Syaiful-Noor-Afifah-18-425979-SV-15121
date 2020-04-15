6cb0646e989ddf18a213baac9f31eb7c8e0212aa
hafifaroon <syaiful.noor.afifah@mail.ugm.ac.id>
2020-04-15 17:04:57 +0700

    Updates

---
+++ precache-manifest.bd19935df63f291d4ee14815bd2e26ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20135b40b020877ba04fef24e90c1b3",
+    "url": "/UTS-Praktikum-Web-3-Syaiful-Noor-Afifah-18-425979-SV-15121/index.html"
+  },
+  {
+    "revision": "977aad02201a35db8420",
+    "url": "/UTS-Praktikum-Web-3-Syaiful-Noor-Afifah-18-425979-SV-15121/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "290886bbd890bcb4991f",
+    "url": "/UTS-Praktikum-Web-3-Syaiful-Noor-Afifah-18-425979-SV-15121/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "977aad02201a35db8420",
+    "url": "/UTS-Praktikum-Web-3-Syaiful-Noor-Afifah-18-425979-SV-15121/static/js/2.d2dc0e04.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/UTS-Praktikum-Web-3-Syaiful-Noor-Afifah-18-425979-SV-15121/static/js/2.d2dc0e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290886bbd890bcb4991f",
+    "url": "/UTS-Praktikum-Web-3-Syaiful-Noor-Afifah-18-425979-SV-15121/static/js/main.7e55d3c7.chunk.js"
+  },
+  {
+    "revision": "6a3518cc21c72853261b",
+    "url": "/UTS-Praktikum-Web-3-Syaiful-Noor-Afifah-18-425979-SV-15121/static/js/runtime-main.7349a780.js"
+  }
+]);
